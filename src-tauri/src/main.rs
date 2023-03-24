afea9271a445d18cbfdcc18f6f377a49011c7261
Adam Waxman <awaxman11@gmail.com>
2023-03-23 21:13:34 -0400

    initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,36 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+use tauri::{ Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+
+fn main() {
+    let system_tray_menu = SystemTrayMenu::new();
+    tauri::Builder::default()
+        .system_tray(SystemTray::new().with_menu(system_tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                // toggle application window
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            },
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application")
+    }
