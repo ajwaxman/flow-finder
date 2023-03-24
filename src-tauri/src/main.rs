6c76f535b7e4c49d1fddc79491fbe31904384723
Adam Waxman <awaxman11@gmail.com>
2023-03-23 22:09:14 -0400

    Center window and close when clicking outside window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,18 +8,25 @@ fn greet(name: &str) -> String {
 }
 
 use tauri::{ Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri_plugin_positioner::{Position, WindowExt};
 
 fn main() {
     let system_tray_menu = SystemTrayMenu::new();
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
-        .on_system_tray_event(|app, event| match event {
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
             SystemTrayEvent::LeftClick {
                 position: _,
                 size: _,
                 ..
             } => {
                 let window = app.get_window("main").unwrap();
+                // use TrayCenter as initial window position
+                let _ = window.move_window(Position::TrayCenter);
+                
                 // toggle application window
                 if window.is_visible().unwrap() {
                     window.hide().unwrap();
@@ -28,6 +35,15 @@ fn main() {
                     window.set_focus().unwrap();
                 }
             },
+            _ => {}
+        }})
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(is_focused) => {
+                // detect click outside of the focused window and hide the app
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
             _ => {}
         })
         .invoke_handler(tauri::generate_handler![greet])
